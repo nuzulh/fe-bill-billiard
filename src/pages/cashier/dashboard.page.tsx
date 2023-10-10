628b221fb105da805ec351a1c616542e6f35211e
nuzulh <nuzulhikam0@gmail.com>
2023-10-10 17:29:51 +0700

    askfasdjf

--- src/pages/cashier/dashboard.page.tsx
+++ src/pages/cashier/dashboard.page.tsx
@@ -3,39 +3,35 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useSearchParams } from "react-router-dom";
 
 export default function DashboardPage() {
-  const [searchParams, setSearchParams] = useSearchParams({
-    tab: "billing",
-  });
-  const tabMenu = searchParams.get("tab");
+  const [searchParams, setSearchParams] = useSearchParams();
+  const tabMenu = searchParams.get("tab") ?? "billing";
+
+  function onChangeParams(key: string, value: string) {
+    setSearchParams((prev) => {
+      prev.set(key, value);
+      return prev;
+    });
+  }
 
   return (
     <main className="container flex flex-col">
-      <Tabs defaultValue={tabMenu ?? "billing"} className="space-y-4">
+      <Tabs defaultValue={tabMenu} className="space-y-4">
         <TabsList className="gap-2">
           <TabsTrigger
             value="billing"
-            onClick={() => setSearchParams((prev) => {
-              prev.set("tab", "billing");
-              return prev;
-            })}
+            onClick={() => onChangeParams("tab", "billing")}
           >
             Billing
           </TabsTrigger>
           <TabsTrigger
             value="fnb"
-            onClick={() => setSearchParams((prev) => {
-              prev.set("tab", "fnb");
-              return prev;
-            })}
+            onClick={() => onChangeParams("tab", "fnb")}
           >
             F&B
           </TabsTrigger>
           <TabsTrigger
             value="order"
-            onClick={() => setSearchParams((prev) => {
-              prev.set("tab", "order");
-              return prev;
-            })}
+            onClick={() => onChangeParams("tab", "order")}
           >
             Order
           </TabsTrigger>
